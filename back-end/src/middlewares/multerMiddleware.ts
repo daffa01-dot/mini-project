9e3f300753f5f94ddccbc0f1c8aa5fae76a84f5d
said muhammad daffa <saiddaffa123@gmail.com>
2026-07-20 02:31:01 +0700

    fix: vercel express entry point

--- back-end/src/middlewares/multerMiddleware.ts
+++ back-end/src/middlewares/multerMiddleware.ts
@@ -1,49 +1,18 @@
 import { Request } from "express";
-import multer, { FileFilterCallback, Multer, StorageEngine } from "multer";
+import multer, { FileFilterCallback, Multer } from "multer";
 import path from "path";
-import fs from "fs";
 import { ResponseError } from "../utils/response-error.util";
 import { StatusCodes } from "http-status-codes";
 
-const storage = multer.memoryStorage();
-
 export const uploadImage = multer({
-  storage,
-  limits: { fileSize: 5 * 1024 * 1024 },
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
 
 export class MulterMiddleware {
-  private allowedExtensions: string[] = [".jpg", ".jpeg", ".png"];
-  private storageType: "diskStorage" | "memoryStorage" = "diskStorage";
-  private uploadDir = path.join(process.cwd(), "public/uploads/resi");
-
-  constructor(storageType?: "diskStorage" | "memoryStorage") {
-    if (storageType) {
-      this.storageType = storageType;
-    }
-
-    if (this.storageType === "diskStorage" && !fs.existsSync(this.uploadDir)) {
-      fs.mkdirSync(this.uploadDir, { recursive: true });
-    }
-  }
-
-  private storage(): StorageEngine {
-    if (this.storageType === "diskStorage") {
-      return multer.diskStorage({
-        destination: (req: Request, file: Express.Multer.File, cb) => {
-          cb(null, this.uploadDir);
-        },
-        filename: (req: Request, file: Express.Multer.File, cb) => {
-          const uniqueSuffix =
-            Date.now() + "-" + Math.round(Math.random() * 1e9);
-          const fileExtension = path.extname(file.originalname);
-          cb(null, `resi-${uniqueSuffix}${fileExtension}`);
-        },
-      });
-    }
-
-    return multer.memoryStorage();
-  }
+  private allowedExtensions = [".jpg", ".jpeg", ".png"];
 
   private fileFilter(
     req: Request,
@@ -52,21 +21,21 @@ export class MulterMiddleware {
   ) {
     const ext = path.extname(file.originalname).toLowerCase();
 
-    if (this.allowedExtensions.includes(ext)) {
-      return cb(null, true);
+    if (!this.allowedExtensions.includes(ext)) {
+      return cb(
+        new ResponseError(
+          StatusCodes.NOT_ACCEPTABLE,
+          `Format file tidak valid untuk ${file.originalname}. Hanya boleh .jpg, .jpeg, .png`,
+        ) as any,
+      );
     }
 
-    return cb(
-      new ResponseError(
-        StatusCodes.NOT_ACCEPTABLE,
-        `Format file tidak valid untuk ${file.originalname}. Hanya boleh .jpg, .jpeg, .png`,
-      ) as any,
-    );
+    cb(null, true);
   }
 
   public upload(limitFileSize: number): Multer {
     return multer({
-      storage: this.storage(),
+      storage: multer.memoryStorage(),
       fileFilter: this.fileFilter.bind(this),
       limits: {
         fileSize: limitFileSize,
