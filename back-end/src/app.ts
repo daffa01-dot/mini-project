d45e92165e10c5d72bb99a4696a2230344e2123e
said muhammad daffa <saiddaffa123@gmail.com>
2026-07-20 20:53:28 +0700

    fix vercel rewrite

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -30,17 +30,7 @@ app.use((req, _res, next) => {
 
 app.use(
   cors({
-    origin(origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (WHITE_LIST?.includes(origin)) {
-        return callback(null, true);
-      }
-
-      console.log("Blocked Origin:", origin);
-
-      callback(new Error("Not allowed by CORS"));
-    },
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
