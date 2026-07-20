9fea131f50f07c00935dcc9120029d7ef28b2e79
said muhammad daffa <saiddaffa123@gmail.com>
2026-07-20 22:25:02 +0700

    fix vercel rewrite

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -30,13 +30,20 @@ app.use((req, _res, next) => {
 
 app.use(
   cors({
-    origin: true,
+    origin: (origin, callback) => {
+      console.log("ORIGIN =", origin);
+
+      callback(null, true);
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
-
+app.use((req, res, next) => {
+  console.log("Access-Control-Allow-Origin:", res.getHeader("Access-Control-Allow-Origin"));
+  next();
+});
 // Menjawab semua preflight
 app.options(/.*/, cors());
 
@@ -44,14 +51,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  "/uploads",
-  express.static(path.join(process.cwd(), "public/uploads"))
-);
+app.use("/uploads", express.static(path.join(process.cwd(), "public/uploads")));
 
 app.use(
   "/api/v1/uploads",
-  express.static(path.join(process.cwd(), "public/uploads"))
+  express.static(path.join(process.cwd(), "public/uploads")),
 );
 
 // =========================
@@ -83,8 +87,6 @@ app.use("/api/v1/satwa", satwaRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/shelter", shelterRouter);
 
-
-
 app.use((_req, res) => {
   res.status(404).json({
     success: false,
@@ -92,18 +94,14 @@ app.use((_req, res) => {
   });
 });
 
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err);
 
-
-app.use(
-  (err: any, _req: Request, res: Response, _next: NextFunction) => {
-    console.error(err);
-
-    res.status(err?.status || StatusCodes.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err?.message || "Internal Server Error",
-      data: null,
-    });
-  }
-);
+  res.status(err?.status || StatusCodes.INTERNAL_SERVER_ERROR).json({
+    success: false,
+    message: err?.message || "Internal Server Error",
+    data: null,
+  });
+});
 
 export default app;
